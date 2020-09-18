263efcb349152d64f2798cdb7521b3b4b0a9ae16
Your Name <you@example.com>
2020-09-19 06:21:17 +0800

    add theidea

---
+++ src/main/java/com/lsk/test/Main.java
@@ -0,0 +1,14 @@
+package com.lsk.test;
+
+/**
+ * TODO
+ *
+ * @author shikai.liu
+ * @version 1.0
+ * @date 2020/9/19 0019 6:18
+ */
+public class Main {
+    public static void main(String[]args){
+        System.out.println("test");
+    }
+}
